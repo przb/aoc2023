00d2bdd39c2f4dda6c21df94a87b3ea63685423f
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-12-06 13:20:00 +1100

    solve day 1

---
+++ src/day01.rs
@@ -0,0 +1,79 @@
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+const DEC_RADIX: u32 = 10;
+const BAD_NUM: u32 = 99;
+
+fn parse_num_str(s: &str) -> Option<u32> {
+    match s {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
+    }
+}
+
+fn parse_num_string(s: &String) -> Option<u32> {
+    match s.len() {
+        3 => parse_num_str(s.as_str()),
+        4 => parse_num_str(s.as_str()).or_else(|| parse_num_str(&s[1..])),
+        5 => parse_num_str(s.as_str())
+            .or_else(|| parse_num_str(&s[1..]))
+            .or_else(|| parse_num_str(&s[2..])),
+        _ => None,
+    }
+}
+
+pub(crate) fn part_one() -> u32 {
+    let input = PathBuf::from_str("inputs/01.txt").unwrap();
+
+    fs::read_to_string(input)
+        .unwrap()
+        .lines()
+        .map(|l| {
+            let mut digits = l.chars().filter(|c| c.is_ascii_digit());
+            let first = digits.next().unwrap_or_default();
+            let last = digits.last().unwrap_or(first);
+            first.to_digit(DEC_RADIX).unwrap() * DEC_RADIX + last.to_digit(DEC_RADIX).unwrap()
+        })
+        .sum()
+}
+
+pub(crate) fn part_two() -> u32 {
+    let input = PathBuf::from_str("inputs/01.txt").unwrap();
+
+    let file = fs::read_to_string(input).unwrap();
+    file.lines()
+        .map(|line| {
+            let mut state = String::default();
+            line.chars()
+                .map(move |c| {
+                    if c.is_ascii_digit() {
+                        c.to_digit(DEC_RADIX).unwrap()
+                    } else {
+                        state.push(c);
+                        if state.len() > 5 {
+                            state.remove(0);
+                        };
+                        parse_num_string(&state).unwrap_or(BAD_NUM)
+                    }
+                })
+                .filter(|v| *v != BAD_NUM)
+            // .collect_vec()
+        })
+        .map(|mut v| {
+            let first = v.next().unwrap_or_default();
+            let last = v.last().unwrap_or(first);
+            // let last = v.into_iter().last().unwrap_or(first);
+            (first, last)
+        })
+        .map(|(l, r)| (l * DEC_RADIX) + r)
+        .sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 use std::error::Error;
-use std::fs;
-use std::path::PathBuf;
-use std::str::FromStr;
+use itertools::Itertools;
 
-const DEC_RADIX: u32 = 10;
+mod day01;
+
+fn run_all() {}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = PathBuf::from_str("inputs/01.txt")?;
+    if let Some(_v) = std::env::args().nth(1) {
+        run_all();
+    } else {
+        let sum = day01::part_two();
+        println!("calibration: {sum}");
+    }
 
-    let sum: u32 = fs::read_to_string(input)?
-        .lines()
-        .map(|l| {
-            let mut digits = l.chars().filter(|c| c.is_ascii_digit());
-            let first = digits.next().unwrap_or_default();
-            let last = digits.last().unwrap_or(first);
-            first.to_digit(DEC_RADIX).unwrap() * DEC_RADIX + last.to_digit(DEC_RADIX).unwrap()
-        })
-        .sum();
-
-    println!("calibration: {sum}");
 
     Ok(())
 }
