07e6d26ba7191dad9984a531f9177541ebfb14bc
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-12-07 21:59:03 +1100

    solve day 4 part 1

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -1,12 +1,44 @@
+use itertools::Itertools;
 use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr;
+
 pub(crate) fn part_one() -> i32 {
-    let filename = PathBuf::from_str("inputs/03.txt").unwrap();
+    let filename = PathBuf::from_str("inputs/04.txt").unwrap();
     let input = fs::read_to_string(filename).unwrap();
 
-    0
+    input
+        .lines()
+        .map(|line| line.split_once(':').unwrap().1)
+        .map(|game| game.split_once('|').unwrap())
+        .map(|(winning_nums, my_nums)| {
+            (
+                winning_nums
+                    .trim()
+                    .split_ascii_whitespace()
+                    .map(|n| n.parse::<i32>().unwrap()),
+                my_nums
+                    .trim()
+                    .split_ascii_whitespace()
+                    .map(|n| n.parse::<i32>().unwrap()),
+            )
+        })
+        .map(|(win_nums, my_nums)| {
+            my_nums.fold(0, |acc, val| {
+                if win_nums.clone().contains(&val) {
+                    if acc == 0 {
+                        1
+                    } else {
+                        acc * 2
+                    }
+                } else {
+                    acc
+                }
+            })
+        })
+        .sum()
 }
+
 pub(crate) fn part_two() -> i32 {
     let filename = PathBuf::from_str("inputs/03.txt").unwrap();
     let input = fs::read_to_string(filename).unwrap();
