65fa318f3bdbfa204e1b763d1ecae6fee1564c7f
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-12-09 22:52:45 +1100

    add timing to csv file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use prettytable::{row, Table};
 use std::error::Error;
+use std::fs::File;
+use std::path::PathBuf;
 
 mod solutions;
 #[cfg(test)]
@@ -7,11 +10,40 @@ mod test;
 use crate::solutions::{day01, day02, day03, day04, day05, Solution};
 
 fn run_all() {
-    day01::Day01.time_both();
-    day02::Day02.time_both();
-    day03::Day03.time_both();
-    day04::Day04.time_both();
-    day05::Day05.time_both();
+    let mut table = Table::new();
+
+    // Add a row per time
+    table.add_row(row!["Day Num", "Part 1 Duration", "Part 2 Duration"]);
+    // A more complicated way to add a row:
+
+    let (d1, d2) = day01::Day01.time_both();
+    table.add_row(row!["1", format!("{d1:?}"), format!("{d2:?}")]);
+    println!("done with day 1");
+
+    let (d1, d2) = day02::Day02.time_both();
+    table.add_row(row!["2", format!("{d1:?}"), format!("{d2:?}")]);
+    println!("done with day 2");
+
+    let (d1, d2) = day03::Day03.time_both();
+    table.add_row(row!["3", format!("{d1:?}"), format!("{d2:?}")]);
+    println!("done with day 3");
+
+    let (d1, d2) = day04::Day04.time_both();
+    table.add_row(row!["4", format!("{d1:?}"), format!("{d2:?}")]);
+    println!("done with day 4");
+
+    let (d1, d2) = day05::Day05.time_both();
+    table.add_row(row!["5", format!("{d1:?}"), format!("{d2:?}")]);
+    println!("done with day 5");
+
+    table.printstd();
+    let time_results_filename = PathBuf::from("times.csv");
+    let file = File::create(time_results_filename.clone()).expect("error creating file");
+
+    match table.to_csv(file) {
+        Ok(_) => println!("saved results to {:?}", time_results_filename),
+        Err(_) => println!("error saving csv"),
+    };
 }
 
 fn run_current() {
--- src/solutions.rs
+++ src/solutions.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::path::PathBuf;
+use std::time::Duration;
 
 pub(crate) mod day01;
 pub(crate) mod day02;
@@ -17,23 +18,25 @@ pub(crate) trait Solution {
         let filename = PathBuf::from(format!("inputs/{:0>2}.txt", Self::DAY_NUM));
         fs::read_to_string(filename).unwrap()
     }
-    fn time_both(&self) {
+    fn time_both(&self) -> (Duration, Duration) {
         let t1 = std::time::SystemTime::now();
         let _ = self.part_one();
         let t2 = std::time::SystemTime::now();
-        println!(
-            "Day {} part 1 took {:?}",
-            Self::DAY_NUM,
-            t2.duration_since(t1).unwrap()
-        );
+
+        let d1 = t2.duration_since(t1).expect("Timing error");
 
         let t1 = std::time::SystemTime::now();
         let _ = self.part_two();
         let t2 = std::time::SystemTime::now();
-        println!(
-            "Day {} part 2 took {:?}",
-            Self::DAY_NUM,
-            t2.duration_since(t1).unwrap()
-        );
+        let d2 = t2.duration_since(t1).expect("Timing error");
+
+        (d1, d2)
+    }
+    fn print_time(&self) {
+        let (duration_one, duration_two) = self.time_both();
+
+        println!("Day {} part 1 took {:?}", Self::DAY_NUM, duration_one);
+
+        println!("Day {} part 2 took {:?}", Self::DAY_NUM, duration_two);
     }
 }
