2ab72d489be39986b90d3554c0ab58600d823b8b
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-12-18 14:29:41 +1100

    run cargo fmt

--- src/solutions/string_grid.rs
+++ src/solutions/string_grid.rs
@@ -16,24 +16,44 @@ pub fn find_line_len(input: &str) -> usize {
 pub fn adjacent_indexes(input: &String, index: usize, include_diagonals: bool) -> Vec<usize> {
     let mut indexes = vec![];
     let line_len = find_line_len(input);
-    let index_less_one = if index.checked_sub(1).is_some_and(|i| !is_newline(input, i)) { Some(index - 1) } else { None };
-    let index_plus_one = if is_newline(input, index + 1) { None } else { Some(index + 1) };
+    let index_less_one = if index.checked_sub(1).is_some_and(|i| !is_newline(input, i)) {
+        Some(index - 1)
+    } else {
+        None
+    };
+    let index_plus_one = if is_newline(input, index + 1) {
+        None
+    } else {
+        Some(index + 1)
+    };
 
-    if let Some(i) = index.checked_sub(line_len) { indexes.push(i) }
-    if is_in_bounds(input, index + line_len) { indexes.push(index + line_len) }
+    if let Some(i) = index.checked_sub(line_len) {
+        indexes.push(i)
+    }
+    if is_in_bounds(input, index + line_len) {
+        indexes.push(index + line_len)
+    }
 
     if let Some(i) = index_less_one {
         indexes.push(i);
         if include_diagonals {
-            if let Some(i_less_row) = i.checked_sub(line_len) { indexes.push(i_less_row) }
-            if is_in_bounds(input, i + line_len) { indexes.push(i + line_len) }
+            if let Some(i_less_row) = i.checked_sub(line_len) {
+                indexes.push(i_less_row)
+            }
+            if is_in_bounds(input, i + line_len) {
+                indexes.push(i + line_len)
+            }
         }
     }
     if let Some(i) = index_plus_one {
         indexes.push(i);
         if include_diagonals {
-            if let Some(i_less_row) = i.checked_sub(line_len) { indexes.push(i_less_row) }
-            if is_in_bounds(input, i + line_len) { indexes.push(i + line_len) }
+            if let Some(i_less_row) = i.checked_sub(line_len) {
+                indexes.push(i_less_row)
+            }
+            if is_in_bounds(input, i + line_len) {
+                indexes.push(i + line_len)
+            }
         }
     }
 
@@ -45,92 +65,91 @@ mod test {
 
     #[test]
     fn simple_grid() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 5, true), vec![0, 1, 2, 4, 6, 8, 9, 10])
     }
 
     #[test]
     fn simple_grid_h_edge() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 1, true), vec![0, 2, 4, 5, 6])
     }
 
     #[test]
     fn simple_grid_corner() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 0, true), vec![1, 4, 5])
     }
 
     #[test]
     fn simple_grid_v_edge() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 4, true), vec![0, 1, 5, 8, 9])
     }
 
     #[test]
     fn simple_grid_alt_corner() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 10, true), vec![5, 6, 9])
     }
 
-
     #[test]
     fn simple_grid_no_diag() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 5, false), vec![1, 4, 6, 9])
     }
 
     #[test]
     fn simple_grid_h_edge_no_diag() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 1, false), vec![0, 2, 5])
     }
 
     #[test]
     fn simple_grid_corner_no_diag() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 0, false), vec![1, 4])
     }
 
     #[test]
     fn simple_grid_v_edge_no_diag() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 4, false), vec![0, 5, 8])
     }
 
     #[test]
     fn simple_grid_alt_corner_no_diag() {
-        let s =
-            "123\n\
+        let s = "123\n\
              456\n\
-             789\n".to_string();
+             789\n"
+            .to_string();
         assert_eq!(adjacent_indexes(&s, 10, false), vec![6, 9])
     }
 }
