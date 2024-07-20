2ebd8d8c8dd46b00869128b1c6b604ac79abcfe0
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-07-19 19:44:31 -0500

    refactor part1 to use a galaxy struct

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -4,6 +4,14 @@ use itertools::Itertools;
 
 pub(crate) struct Day11;
 
+struct Galaxy {
+    // starting at zero from the top left
+    x: i32,
+    y: i32,
+    index: usize,
+    num: usize,
+}
+
 fn get_expanded_rows(input: &String) -> Vec<usize> {
     input
         .lines()
@@ -29,39 +37,42 @@ fn get_expanded_cols(input: &String) -> Vec<usize> {
         .collect()
 }
 
-fn get_galaxy_locs(input: &String) -> Vec<usize> {
+fn get_galaxies(input: &String, line_len: usize) -> Vec<Galaxy> {
     input
         .chars()
-        .filter(|c| *c != '\n')
+        .filter(|c| *c != '\n') // filter out the new line chars
+        // this enumerate counts the position of the galaxy in the string (excluding newlines)
         .enumerate()
-        .filter_map(|(i, c)| (c == '#').then(|| i))
+        .filter(|(_, c)|*c == '#') // only include the galaxy chars
+        // this enumerate counts the number of galaxies
+        .enumerate()
+        .map(|(num, (index, c))| Galaxy {
+            num,
+            index,
+            x: (index % line_len) as i32,
+            y: (index / line_len) as i32,
+        })
         .collect()
 }
 
 fn get_manhattan_distance(
-    i: usize,
-    j: usize,
+    i: &Galaxy,
+    j: &Galaxy,
     expanded_rows: &[usize],
     expanded_cols: &[usize],
-    line_len: usize,
 ) -> i32 {
-    let xi = (i % line_len) as i32;
-    let yi = (i / line_len) as i32;
-    let xj = (j % line_len) as i32;
-    let yj = (j / line_len) as i32;
+    let x_r = min(i.x, j.x)..max(i.x, j.x);
+    let y_r = min(i.y, j.y)..max(i.y, j.y);
 
-    let x_r = min(xi, xj)..max(xi, xj);
-    let y_r = min(yi, yj)..max(yi, yj);
-
-    let mut dist = i32::abs(xi - xj);
-    dist += i32::abs(yi - yj);
+    let mut dist = i32::abs(i.x - j.x);
+    dist += i32::abs(i.y - j.y);
     dist += expanded_rows
         .iter()
-        .filter(|r| y_r.contains(&(**r as i32)))
+        .filter(|row| y_r.contains(&(**row as i32)))
         .count() as i32;
     dist += expanded_cols
         .iter()
-        .filter(|c| x_r.contains(&(**c as i32)))
+        .filter(|col| x_r.contains(&(**col as i32)))
         .count() as i32;
     // println!("dist: {dist}");
     dist
@@ -73,27 +84,27 @@ impl Solution for Day11 {
 
     fn part_one(&self) -> Self::ReturnType {
         let input = self.get_input();
-//         let input = "...#......
-// .......#..
-// #.........
-// ..........
-// ......#...
-// .#........
-// .........#
-// ..........
-// .......#..
-// #...#....."
-//             .to_string();
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."
+            .to_string();
 
         let line_len = input.find('\n').unwrap();
-        let galaxies = get_galaxy_locs(&input);
+        let galaxies = get_galaxies(&input, line_len);
         let expanded_rows = get_expanded_rows(&input);
         let expanded_cols = get_expanded_cols(&input);
 
         galaxies
             .iter()
             .tuple_combinations()
-            .map(|(i, j)| get_manhattan_distance(*i, *j, &expanded_rows, &expanded_cols, line_len))
+            .map(|(i, j)| get_manhattan_distance(i, j, &expanded_rows, &expanded_cols))
             .sum()
     }
 
