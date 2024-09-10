c4a73e4ac66ee12c6ba12494242e96c926e11b89
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-09-09 19:18:10 -0500

    use somebody elses code to solve day 12 part two

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,7 +1,8 @@
-use itertools::Itertools;
-
 use crate::solutions::day12::SpringStatus::{Broken, Operational, Unknown};
 use crate::solutions::Solution;
+use itertools::Itertools;
+use rayon::prelude::*;
+use std::collections::HashMap;
 
 pub(crate) struct Day12;
 
@@ -29,16 +30,7 @@ impl TryFrom<char> for SpringStatus {
     }
 }
 
-fn expanded_line(line: &str) -> String {
-    line.chars()
-        .chain(line.chars())
-        .chain(line.chars())
-        .chain(line.chars())
-        .chain(line.chars())
-        .collect()
-}
-
-fn count(cfg: &[u8], nums: &[usize]) -> i32 {
+fn count(cfg: &[u8], nums: &[usize]) -> u32 {
     if cfg.len() == 0 {
         if nums.len() == 0 {
             1
@@ -79,9 +71,60 @@ fn count(cfg: &[u8], nums: &[usize]) -> i32 {
     }
 }
 
+fn dp_count<'a>(
+    cfg: &'a [u8],
+    nums: &'a [usize],
+    cache: &mut HashMap<(&'a [u8], &'a [usize]), u64>,
+) -> u64 {
+    if cfg.len() == 0 {
+        if nums.len() == 0 {
+            1
+        } else {
+            0
+        }
+    } else if nums.len() == 0 {
+        if cfg.contains(&(BROKEN_CHAR as u8)) {
+            0
+        } else {
+            1
+        }
+    } else {
+        let key = (cfg, nums);
+
+        match cache.get(&key) {
+            Some(val) => *val,
+            None => {
+                let len = cfg.len();
+                let mut result = 0;
+                let first_char = cfg[0];
+                if ".?".as_bytes().contains(&first_char) {
+                    result += dp_count(&cfg[1..len], nums, cache);
+                }
+
+                if "#?".as_bytes().contains(&first_char) {
+                    let first_num = nums[0] as usize;
+                    let functional = &(OPERATIONAL_CHAR as u8);
+                    let broken = BROKEN_CHAR as u8;
+                    if first_num <= len
+                        && !cfg[0..first_num].contains(functional)
+                        && (first_num == len || cfg[first_num] != broken)
+                    {
+                        let start_range = usize::min(first_num + 1, len);
+                        let new_cfg = &cfg[start_range..len];
+                        result += dp_count(&new_cfg, &nums[1..nums.len()], cache);
+                    } else {
+                        result += 0;
+                    }
+                }
+                cache.insert(key, result);
+                result
+            }
+        }
+    }
+}
 impl Solution for Day12 {
     const DAY_NUM: i32 = 12;
-    type ReturnType = i32;
+    type ReturnType = u64;
 
     fn part_one(&self) -> Self::ReturnType {
         let input = self.get_input();
@@ -90,7 +133,7 @@ impl Solution for Day12 {
             .map(|line| {
                 let (cfg, nums) = line.split_once(" ").unwrap();
                 let nums = nums.split(',').map(|c| c.parse().unwrap()).collect_vec();
-                count(cfg.as_bytes(), &nums) as i32
+                count(cfg.as_bytes(), &nums) as u64
             })
             .sum()
     }
@@ -98,20 +141,16 @@ impl Solution for Day12 {
     fn part_two(&self) -> Self::ReturnType {
         let input = self.get_input();
 
-        for line in input.lines() {
-            let (states, counts) = line.split_once(" ").unwrap();
-            let states = expanded_line(states);
-            //println!("states: {:?}", states);
-            //let counts = expanded_line(counts);
-            let states: Vec<SpringStatus> = states.chars().map(|c| c.try_into().unwrap()).collect();
-            let counts: Vec<usize> = counts.split(",").map(|c| c.parse().unwrap()).collect();
-
-            let states = states.iter().dedup_with_count().collect_vec();
-
-            //println!("line: {line}");
-            //println!("counts: {:?}", counts);
-            //println!();
-        }
-        0
+        input
+            .par_lines()
+            .map(|line| {
+                let (cfg, nums) = line.split_once(" ").unwrap();
+                let cfg = Vec::from([cfg, cfg, cfg, cfg]).join("?");
+                let nums = Vec::from([nums, nums, nums, nums, nums]).join(",");
+                let nums = nums.split(",").map(|c| c.parse().unwrap()).collect_vec();
+                let mut cache = HashMap::new();
+                dp_count(cfg.as_bytes(), &nums, &mut cache)
+            })
+            .sum()
     }
 }
